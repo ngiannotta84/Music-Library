37f259dd5d2401e7b53d8db9277229d4568fa9ad
nicola giannotta <nicolagiannotta84@gmail.com>
2022-08-18 18:37:32 +0100

    set up and fist test

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require('./src/app')
+const APP_PORT = process.env.PORT || 4000
+
+app.listen(APP_PORT, () => {
+  console.log(`App is listening on port ${APP_PORT}`)
+})
+app.get('/', (req, res) => {
+  res.status(200).json('Hello World')
+})
