b2fa8e968bef1ac7ac1d7875710ffe17e7931eb7
nicola giannotta <nicolagiannotta84@gmail.com>
2022-08-21 17:12:49 +0100

    single artist search on artists list completed

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -3,11 +3,42 @@ exports.create = async (req,res) => {
     const dbConnection = await getDb();
     const { name, genre } = req.body;
     try {
-   await dbConnection.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+   await dbConnection.query(`INSERT INTO Artist (name, genre) VALUES (?, ?)`,[
+    name,
+    genre,
+   ]);
 
     res.sendStatus(201);
     } catch (err) {
         res.sendStatus(500).json(err);
     }
     dbConnection.end();
+}
+exports.read = async (_,res) => {
+    const dbConnection = await getDb();
+
+    try {
+        const [artist] = await dbConnection.query('SELECT * FROM Artist');
+
+        res.status(200).json(artist);
+    } catch (err) {
+        res.status(500).json(err)   
+    }
+    dbConnection.end();
+}
+exports.readById = async (req,res) => {
+    const dbConnection = await getDb();
+        const { artistId } = req.params;
+      
+        const [[artist]] = await dbConnection.query('SELECT * FROM Artist WHERE id = ?', [
+          artistId,
+        ]);
+      
+        if (!artist) {
+          res.sendStatus(404);
+        } else {
+          res.status(200).json(artist);
+        }
+      
+        dbConnection.end();
 }
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,5 +4,7 @@ const artistController = require('../controllers/artist');
 const router = express.Router();
 
 router.post('/', artistController.create);
+router.get('/', artistController.read);
+router.get('/:artistId',artistController.readById),
 
 module.exports = router;
